0217cd662cdb0a1812afb1f50d094ccecb227015
sergioarango <sergio.arangogr@gmail.com>
2023-05-05 18:49:25 -0500

    Update CasoIPruebaHipotesisMedia.py

--- CasoIPruebaHipotesisMedia.py
+++ CasoIPruebaHipotesisMedia.py
@@ -24,7 +24,7 @@ Z=(Xbarra-Ybarra)/(mt.sqrt(((sigma1**2)/n)+((sigma2**2)/m)))
 #Prueba de hipótesis con valores Z
 Zalfa=norm.ppf(1-alfa)
 if Z>Zalfa:
-    print("Se rechaza la hipótesis nula, es decir la media de edad de los médicos es superior a la de los médicos académicos")
+    print("Se rechaza la hipótesis nula, es decir la media de edad de los médicos prácticantes es superior a la de los médicos académicos")
 else:
     print("No se rechaza la hipótesis nula, es decir, la media de edad de los médios practicantes No es superior a la de los académicos")
 
